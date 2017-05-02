78e66ad724bdf2035d1326bc9cd016dcf0865e7c
michael-rutledge <mrutledge2808@gmail.com>
2017-05-02 10:11:36 -0700

    Fixed weird ride shader, created separate script to manage drum color

---
+++ Assets/Scripts/DrumColorManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumColorManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // set color of drums based on type
+        switch (this.name)
+        {
+            case "SnareDrum":
+                this.GetComponent<MeshRenderer>().material.color = new Color(1, .75f, .75f, 1);
+                break;
+            case "HiHatImposter":
+                this.GetComponent<MeshRenderer>().material.color = new Color(.99f, .99f, .59f, 1);
+                break;
+            case "CrashImposter":
+                this.GetComponent<MeshRenderer>().material.color = new Color(.47f, .87f, .47f, 1);
+                break;
+            case "RideImposter":
+                this.GetComponent<MeshRenderer>().material.color = new Color(.63f, .77f, 1, 1);
+                break;
+            case "HighTom":
+                this.GetComponent<MeshRenderer>().material.color = new Color(0.0f, 1.0f, 1.0f, 1);
+                break;
+            case "MedTom":
+                this.GetComponent<MeshRenderer>().material.color = new Color(0.0f, 1.0f, 1.0f, 1);
+                break;
+            case "LowTom":
+                this.GetComponent<MeshRenderer>().material.color = new Color(0.0f, 1.0f, 1.0f, 1);
+                break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/DrumTriggerManager.cs
+++ Assets/Scripts/DrumTriggerManager.cs
@@ -25,25 +25,6 @@ public class DrumTriggerManager : MonoBehaviour {
         rightHit = leftHit = -1.0;
         oldRightPos = stickRightHead.transform.position;
         oldLeftPos = stickLeftHead.transform.position;
-        // set color of drums based on type
-        switch (this.name)
-        {
-            case "SnareDrum":
-                this.GetComponent<MeshRenderer>().material.color = new Color(1, .75f, .75f, 1);
-                break;
-            case "HiHatImposter":
-                this.GetComponent<MeshRenderer>().material.color = new Color(.99f, .99f, .59f, 1);
-                break;
-            case "CrashImposter":
-                this.GetComponent<MeshRenderer>().material.color = new Color(.47f, .87f, .47f, 1);
-                break;
-            case "Ride":
-                this.GetComponent<MeshRenderer>().material.color = new Color(.63f, .77f, 1, 1);
-                break;
-            case "HighTom":
-                this.GetComponent<MeshRenderer>().material.color = new Color(0.0f, 1.0f, 1.0f, 1);
-                break;
-        }
     }
 	
 	// Update is called once per frame
